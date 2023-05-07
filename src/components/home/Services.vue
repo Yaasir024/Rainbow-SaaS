5c269513eecc2ecac00e40c6baff5eeca9e005d0
Yaasir024 <yope4ever@gmail.com>
2023-05-07 13:46:39 +0100

    /services-section

---
+++ src/components/home/Services.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="bg-[#45DEFE] py-[70px] px-[20px] flex flex-col items-center">
+    <h3 class="text-[20px] sm:text-[24px] leading-[24px] font-tomato text-center">
+      From One Horizon to Another:
+    </h3>
+    <h2
+      class="text-[30px] sm:text-[48px] leading-[38px] sm:leading-[58px] font-medium font-tomato text-center mt-[24px]"
+    >
+      We Power Venture Scale in Africa
+    </h2>
+    <div class="flex items-center justify-center flex-wrap mt-[48px]">
+      <div class="p-[8px] sm:p-[12px]">
+        <div class="px-[42px] py-[58px] bg-white rounded-[32px]">
+          <img src="@/assets/images/home/services/growth.svg" alt="" class="" />
+          <h4
+            class="text-[24px] leading-[32px] font-semibold mt-[28px] text-rb-primary-900 font-tomato"
+          >
+            Accelerate Growth
+          </h4>
+          <p class="text-base mt-[16px] max-w-[270px]">
+            Rainbow helps you boost your recurring revenue, including upsells,
+            cross-sells, upgrades, and unique subscription options.
+          </p>
+        </div>
+      </div>
+      <div class="p-[8px] sm:p-[12px]">
+        <div class="px-[42px] py-[58px] bg-white rounded-[32px]">
+          <img
+            src="@/assets/images/home/services/paperwork.svg"
+            alt=""
+            class=""
+          />
+          <h4
+            class="text-[24px] leading-[32px] font-semibold mt-[28px] text-rb-primary-900 font-tomato"
+          >
+            Get rid of Paperworks
+          </h4>
+          <p class="text-base mt-[16px] max-w-[270px]">
+            Your entire business operation can be up and running in a matter of
+            days. Rainbow takes care of everything from filing paperwork
+          </p>
+        </div>
+      </div>
+      <div class="p-[8px] sm:p-[12px]">
+        <div class="px-[42px] py-[58px] bg-white rounded-[32px]">
+          <img src="@/assets/images/home/services/risk.svg" alt="" class="" />
+          <h4
+            class="text-[24px] leading-[32px] font-semibold mt-[28px] text-rb-primary-900 font-tomato"
+          >
+            Eliminate Risk
+          </h4>
+          <p class="text-base mt-[16px] max-w-[270px]">
+            Sell locally while preventing fraud, handling chargebacks, and
+            adhering with all current and new regulations.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Hero from "@/components/home/Hero.vue";
 import Products from "@/components/home/Products.vue";
+import Services from "@/components/home/Services.vue";
 </script>
 
 <template>
   <main class="">
     <Hero />
     <Products />
+    <Services />
   </main>
 </template>
 
