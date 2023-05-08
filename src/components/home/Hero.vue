ccc74328a2a5017bb9249c877ac76316c6e557f3
Yaasir024 <yope4ever@gmail.com>
2023-05-08 16:28:35 +0100

    /

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,12 +1,30 @@
 <script setup>
+import { onMounted } from "vue";
+import gsap from "gsap";
 import BoltBtn from "@/components/buttons/TypeBolt.vue";
 import BorderBtn from "@/components/buttons/TypeBorder.vue";
+
+let tl = gsap.timeline({ yoyo: true, defaults: { opacity: 0, ease: "back" } });
+
+const animate = () => {
+  tl.from("#hero-main-text", { y: -80, duration: .6 })
+    .from("#hero-excerpt", { y: -40, duration: .6 }, "-=0.2")
+    .from("#hero-buttons", { y: -25, duration: .6 }, "-=0.2")
+    .from("#badge-btn-1", { y: 50, duration: .6 }, "-=0.6")
+}
+
+onMounted(() => {
+  setTimeout(() => {
+
+    animate()
+  }, '500')
+})
 </script>
 
 <template>
   <section class="hero pt-[145px] overflow-hidden relative min-h-[calc(100vh_-_80px)]">
     <div class="flex flex-col items-center px-[20px] mb-[200px] xs:mb-[300px] md:mb-[350px] xl:mb-[420px]">
-      <RouterLink to="/about" class="" data-aos="zoom-in" data-aos-delay="80">
+      <RouterLink to="/about" class="" id="badge-btn-1">
         <div class="badge-container px-[10px] py-[6px] flex items-center w-fit">
           <button
             class="text-[14px] leading-[20px] font-medium text-rb-dark tracking-[-0.01em] bg-[#EDEDF1] rounded-[20px] py-1 px-2 mr-[8px]">
@@ -18,16 +36,16 @@ import BorderBtn from "@/components/buttons/TypeBorder.vue";
 
       <h1
         class="text-[30px] sm:text-[48px] md:text-[64px] leading-[38px] sm:leading-[54px] md:leading-[68px] tracking-[-0.02em] font-semibold max-w-[835px] mt-[38px] font-tomato text-white text-center"
-        data-aos="fade-down">
+        id="hero-main-text">
         <span class="text-rb-main">Rainbow</span> is revolutionizing Global
         Commerce.
       </h1>
       <p class="text-[16px] sm:text-[20px] leading-[20px] sm:leading-[28px] tracking-[-0.014em] max-w-[580px] mt-[24px] text-white text-center"
-        data-aos-delay="10" data-aos-easing="ease-in-out">
+        id="hero-excerpt">
 
         Enabling global businesses to scale in Africa without paperwork
       </p>
-      <div class="flex items-center mt-[32px]" data-aos-delay="30" data-aos-easing="ease-in-out">
+      <div class="flex items-center mt-[32px]" id="hero-buttons">
         <div class="mr-[16px]">
           <RouterLink to="/">
             <BoltBtn text="get started" />
